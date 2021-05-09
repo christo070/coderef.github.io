ee0d5d6d8858eddf00afa2f427c07f3ab8c0cd19
Christo Sojan <77064772+christo070@users.noreply.github.com>
2021-05-09 10:10:37 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,34 @@
+
+document.getElementById("cardbody1").addEventListener("mouseenter",newwind);
+document.getElementById("header").onclick = newwind2;
+
+function newwind()
+{
+    window.open("https://www.home.sandvik/en/stories/articles/2020/11/digital-technology-on-the-cutting-edge/","_blank");
+}
+function newwind2()
+{
+    window.open("https://www.home.sandvik/en/stories/articles/2019/11/5g-is-finally-here-this-is-what-you-need-to-know/","_blank");
+}
+function newwind3()
+{
+    window.open("https://www.home.sandvik/en/stories/articles/2019/11/5g-is-finally-here-this-is-what-you-need-to-know/","_blank");
+}
+function newwind6()
+{
+    window.open("https://www.home.sandvik/en/stories/articles/2018/02/data-the-next-great-resource/","_blank");
+}
+function newwind4()
+{
+    window.open("https://financialwolves.com/get-paid-to-code-online/","_blank");
+}
+function newwind8()
+{
+    window.open("https://elitedatascience.com/learn-machine-learning","_blank");
+}
+
+
+function newwind7()
+{
+    window.open("https://blog.codingblocks.com/2019/start-with-competitive-programming/","_blank");
+}
